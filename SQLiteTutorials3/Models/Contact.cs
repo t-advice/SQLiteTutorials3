5b59acaff5ef1540c5a234fb0ecaa5145d971f38
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-15 14:26:46 +0200

    contact model completed .

---
+++ SQLiteTutorials3/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace SQLiteTutorials3.Models
+{
+    public  class Contact
+    {
+        // tis defines what a single contact looks like . 4 tables in the database.
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreatedAt { get; set; }=DateTime.Now;
+
+    }
+}
