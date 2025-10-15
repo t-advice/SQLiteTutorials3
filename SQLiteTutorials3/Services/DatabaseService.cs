292e23c8c75b45a139cb93967ae45326cba84d3c
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-15 14:37:23 +0200

    i have created the database service

---
+++ SQLiteTutorials3/Services/DatabaseService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLiteTutorials3.Models;
+
+namespace SQLiteTutorials3.Services
+{
+    
+    public class DatabaseService
+    {
+        private readonly SQLiteAsyncConnection _database;
+        public DatabaseService(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath); // this is the connection to the database.
+            _database.CreateTableAsync<Contact>().Wait(); // this creates the table if it does not exist.
+        }
+
+        public Task<List<Contact>> GetContactAsync()
+        {
+            return _database.Table<Contact>().ToListAsync();
+        }
+        public Task<int> SaveContactAsync(Contact contact)
+        {
+            return _database.InsertOrReplaceAsync(contact);
+        }
+        public Task<int> DeleteContactAsync(Contact contact)
+        {
+            return _database.DeleteAsync(contact);
+        }
+
+    }
+}
